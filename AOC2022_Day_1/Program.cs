726121f76ef87a2f9f2dd09a229d47182856e1c1
Péter Bódizs <peter.bodizs@tekenable.com>
2022-12-02 22:29:47 +0100

    day1 done

--- AOC2022_Day_1/Program.cs
+++ AOC2022_Day_1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AOC2022_Day_1
+﻿using System.Runtime.Serialization;
+
+namespace AOC2022_Day_1
 {
     internal class Program
     {
@@ -7,18 +9,20 @@
             Console.WriteLine("Hello, Advent of Code 2022 Day 01");
 
             var lines = File.ReadAllLines("Input2.txt");
+            List<int> sums = new();
             var max = 0;
             var sum = 0;
 
             foreach (var line in lines)
             {
-                Console.WriteLine($"{line}");
+                //Console.WriteLine($"{line}");
                 if (string.IsNullOrEmpty(line))
                 {
                     if (sum > max)
                     {
                         max = sum;                       
                     }
+                    sums.Add(sum);
                     sum = 0;
                     continue;
                 }
@@ -26,7 +30,17 @@
                 sum += int.Parse(line);
             }
 
+            if (sum > max)
+            {
+                max = sum;
+            }
+            sums.Add(sum);
+
             Console.WriteLine($"The maximum calories are: {max}");
+
+            var topThree = sums.OrderByDescending(x => x).Take(3).Sum();
+            Console.WriteLine($"The top three maximum calories sum is: {topThree}");
+
         }
     }
 }
