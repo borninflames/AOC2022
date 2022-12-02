14eff4968386186d62016d997b1df4c33cb06d51
Péter Bódizs <peter.bodizs@tekenable.com>
2022-12-02 22:15:40 +0100

    Add project files.

---
+++ AOC2022_Day_1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AOC2022_Day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Advent of Code 2022 Day 01");
+
+            var lines = File.ReadAllLines("Input2.txt");
+            var max = 0;
+            var sum = 0;
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine($"{line}");
+                if (string.IsNullOrEmpty(line))
+                {
+                    if (sum > max)
+                    {
+                        max = sum;                       
+                    }
+                    sum = 0;
+                    continue;
+                }
+
+                sum += int.Parse(line);
+            }
+
+            Console.WriteLine($"The maximum calories are: {max}");
+        }
+    }
+}
---
+++ AOC2022_Day_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AOC2022_Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
