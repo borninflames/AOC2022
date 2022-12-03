8da2da7f9a21158ffcf27c2fc61b761c1d3a0ae5
Péter Bódizs <peter.bodizs@tekenable.com>
2022-12-03 14:51:02 +0100

    day3 part 2

--- AOC2022_Day_03/Program.cs
+++ AOC2022_Day_03/Program.cs
@@ -7,7 +7,12 @@
             Console.WriteLine("Hello, Advent of Code 2022 Day 03");
 
             var lines = File.ReadAllLines("Input2.txt");
-            var sum = lines.Sum(line => GetValue(line));
+            var sum = 0;
+            for (int i = 0; i < lines.Length; i+=3)
+            {
+                sum += GetValue2(lines[i], lines[i + 1], lines[i + 2]);
+            }
+
             Console.WriteLine($"The sum is: {sum}");
         }
 
@@ -25,6 +30,24 @@
 
             var common = commons.First();
             var val = common > 90 ? common - 96: common - 38;
+            return val;
+        }
+
+        static int GetValue2(string line1, string line2, string line3)
+        {
+            var commons = line1.Intersect(line2).ToList();
+            if (!commons.Any())
+            {
+                return 0;
+            }
+            commons = commons.Intersect(line3).ToList();
+            if (!commons.Any())
+            {
+                return 0;
+            }
+
+            var common = commons.First();
+            var val = common > 90 ? common - 96 : common - 38;
             return val;
         }
     }
