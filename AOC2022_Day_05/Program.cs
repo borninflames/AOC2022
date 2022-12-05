f275899d5266812a08ef41c150ee246081222709
Péter Bódizs <peter.bodizs@tekenable.com>
2022-12-05 22:18:27 +0100

    day 5 part 1

---
+++ AOC2022_Day_05/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+
+namespace AOC2022_Day_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Advent of Code 2022 Day 04");
+
+            var lines = File.ReadAllLines("Input2.txt");
+            var stacks = CreateStacks(lines);
+            var foundTheFirstOperationLine = false;
+            foreach (var line in lines)
+            {
+                if (line == string.Empty)
+                {
+                    foundTheFirstOperationLine = true;
+                    continue;
+                }
+
+                if (foundTheFirstOperationLine)
+                {
+                    var opParts = line.Split(' ');
+                    var howMany = int.Parse(opParts[1]);
+                    var from = int.Parse(opParts[3]) - 1;
+                    var to = int.Parse(opParts[5]) - 1;
+                    for (int i = 0; i < howMany; i++)
+                    {
+                        stacks[to].Push(stacks[from].Pop());
+                    }
+                }
+            }
+            Console.WriteLine("--------------------------------");
+
+            foreach (var stack in stacks)
+            {
+                Console.Write(stack.Peek());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------");
+
+
+
+
+        }
+
+        static Stack<char>[] CreateStacks(string[] lines)
+        {
+            var stacks = new List<Stack<char>>();
+            List<string> stacksInput = new();
+            var j = 0;
+            while (lines[j] != string.Empty)
+            {
+                stacksInput.Add(lines[j++]);
+            }
+
+            stacksInput.Reverse();
+            stacksInput = stacksInput.Skip(1).ToList();
+            foreach (var level in stacksInput)
+            {
+                for (int i = 1, k = 0; i < level.Length; i += 4, k++)
+                {
+                    if (level[i] != ' ')
+                    {
+                        if (stacks.Count < k + 1)
+                        {
+                            stacks.Add(new());
+                        }
+                        stacks[k].Push(level[i]);
+                    }
+                }
+            }
+
+            return stacks.ToArray();
+        }
+    }
+}
