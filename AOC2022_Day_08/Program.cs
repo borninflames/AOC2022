a86a167ec1156a93e5cd974c58056c390cecfd99
Péter Bódizs <peter.bodizs@tekenable.com>
2022-12-08 13:21:13 +0100

    d8 p2

--- AOC2022_Day_08/Program.cs
+++ AOC2022_Day_08/Program.cs
@@ -8,19 +8,21 @@
             var forest = File.ReadAllLines("Input2.txt");
             var size = forest[0].Length;
             var visibleTrees = size * 4 - 4;
+            var maximumScore = 0;
 
             for (int row = 1; row < size - 1; row++)
             {
                 for (int col = 1; col < size - 1; col++)
                 {
-                    if (IsVisible(forest, row, col))
+                    var score = ScenicScore(forest, row, col);
+                    if (score > maximumScore)
                     {
-                        visibleTrees++;
+                        maximumScore = score;
                     }
                 }
             }
 
-            Console.WriteLine(visibleTrees);
+            Console.WriteLine(maximumScore);
         }
 
         static bool IsVisible(string[] forest, int row, int col)
@@ -29,11 +31,49 @@
             var leftSide = forest[row][..col];
             var rightSide = forest[row][(col + 1)..];
             var above = string.Join("", forest[..row].Select(r => r[col]));
-            var below = string.Join("", forest[(row + 1)..].Select(r => r[col]));            
+            var below = string.Join("", forest[(row + 1)..].Select(r => r[col]));
             return leftSide.All(t => t < tree) ||
                 rightSide.All(t => t < tree) ||
                 above.All(t => t < tree) ||
                 below.All(t => t < tree);
+        }
+
+        static int ScenicScore(string[] forest, int row, int col)
+        {
+            var tree = forest[row][col];
+            var leftSide = forest[row][..col];
+            var rightSide = forest[row][(col + 1)..];
+            var above = string.Join("", forest[..row].Select(r => r[col]));
+            var below = string.Join("", forest[(row + 1)..].Select(r => r[col]));
+
+            var scoreLeft = 0;
+            var scoreAbove = 0;
+            var scoreRight = 0;
+            var scoreBelow = 0;
+            var isViewBlocked = false;
+
+            for (int i = leftSide.Length - 1; i >= 0 && !isViewBlocked; i--, scoreLeft++)
+            {
+                isViewBlocked = leftSide[i] >= tree;
+            }
+            isViewBlocked = false;
+            for (int i = above.Length - 1; i >= 0 && !isViewBlocked; i--, scoreAbove++)
+            {
+                isViewBlocked = above[i] >= tree;
+            }
+            isViewBlocked = false;
+            for (int i = 0; i < rightSide.Length && !isViewBlocked; i++, scoreRight++)
+            {
+                isViewBlocked = rightSide[i] >= tree;
+            }
+            isViewBlocked = false;
+            for (int i = 0; i < below.Length && !isViewBlocked; i++, scoreBelow++)
+            {
+                isViewBlocked = below[i] >= tree;
+            }
+
+            var score = scoreLeft * scoreAbove * scoreRight * scoreBelow;
+            return score;
         }
     }
 }
